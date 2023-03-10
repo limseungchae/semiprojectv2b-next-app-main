bdd4923ceca69762cfa46ea4191730af704d4177
limseungchae <tmdco12@naver.com>
2023-03-10 16:27:30 +0900

    MariaDB용 Board 클래스 구현 7 - 새글쓰기

--- pages/board/write.js
+++ pages/board/write.js
@@ -1,29 +1,16 @@
-import axios from "axios";
 import {useState} from "react";
 
+export default function Write() {
 
-export async function getServerSideProps(ctx) {
-    let bno = ctx.query.bno;
-
-    const url = `http://localhost:3000/api/board/view?bno=${bno}`;
-    const res = await axios.get(url);
-    const board = await res.data[0];
-
-    return { props: {board} }
-}
-
-export default function write() {
-
-   const [title, setTitle] = useState('');
-   const [userid, setUserid] = useState('zzyzzy');
-   const [contents, serContents] = useState('');
-
+    const [title, setTitle] = useState('');
+    const [userid, setUserid] = useState('zzyzzy');
+    const [contents, setContents] = useState('');
 
     return (
         <main>
             <div id="main">
                 <h2>게시판 새글쓰기</h2>
-                <form name="write" id="writefrm" />
+                <form name="write" id="writefrm">
                     <div><label htmlFor="title">제목</label>
                         <input type="text" name="title" id="title" /></div>
 
@@ -33,12 +20,13 @@ export default function write() {
 
                     <div><label htmlFor="contents" className="drgup">본문</label>
                         <textarea name="contents" id="contents"
-                                  rows="7" cols="55" /></div>
+                                  rows="7" cols="55"></textarea></div>
 
                     <div><label></label>
                         <button type="button" id="writebtn">입력완료</button>
                         <button type="reset">다시입력</button>
                     </div>
+                </form>
             </div>
         </main>
     );
