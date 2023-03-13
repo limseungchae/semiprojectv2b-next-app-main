d626b8673f57ed21716b63311c870aa860ef504c
limseungchae <tmdco12@naver.com>
2023-03-13 11:47:21 +0900

    구글 reacptcha 인증 확인

---
+++ pages/api/board/recaptcha.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export default async (req, res) => {
+    let params = '?secret=' + process.env.SECRET_KEY;
+    params += '&response=' + req.query.response;
+
+    let url = 'https:/http://localhost:63342/recaptcha/api/siteverify';
+    url = url + params;
+
+    const data = axios.get(url).then(data => data.data);
+    console.log(await data);
+
+    res.status(200).json((await data).success);
+}
